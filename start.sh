02fe66ff09a413adb64ebc76c9f240171d6e6249
Robert Boerwinkle <rfboerwinkle@gmail.com>
2024-02-29 19:29:12 -0600

    log date + cron compatible
    For some reason bash through cron didn't like the array?

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 ip="192.168.100.10"
-ports=("1181" "1182" "1183", "1184")
 video_res="320x240"
 extra_flags=" -loglevel warning"
 encoding=""
 
-ffmpeg $extra_flags -video_size $video_res -i /dev/video0 $encoding -f mpegts udp://$ip:${ports[0]} &
-ffmpeg $extra_flags -video_size $video_res -i /dev/video2 $encoding -f mpegts udp://$ip:${ports[1]} &
-ffmpeg $extra_flags -video_size $video_res -i /dev/video4 $encoding -f mpegts udp://$ip:${ports[2]} &
-ffmpeg $extra_flags -video_size $video_res -i /dev/video6 $encoding -f mpegts udp://$ip:${ports[3]} &
+ffmpeg $extra_flags -video_size $video_res -i /dev/video0 $encoding -f mpegts udp://$ip:1181 &
+ffmpeg $extra_flags -video_size $video_res -i /dev/video2 $encoding -f mpegts udp://$ip:1182 &
+ffmpeg $extra_flags -video_size $video_res -i /dev/video4 $encoding -f mpegts udp://$ip:1183 &
+ffmpeg $extra_flags -video_size $video_res -i /dev/video6 $encoding -f mpegts udp://$ip:1184 &
 
+date
 echo "streams running as background process, enter 'sudo killall ffmpeg' to stop them"
