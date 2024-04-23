50c899e8c16d8d866aaeb90798bf390d177e4c9c
Robert Boerwinkle <rfboerwinkle@gmail.com>
2024-04-22 19:59:44 -0500

    removed testing code

--- start.sh
+++ start.sh
@@ -3,15 +3,15 @@
 declare -a argumentList
 argumentList=()
 declare -a portList
-portList=("1181" "1182" "1183" "1184")
+portList=("1185" "1186" "1187" "1188")
 # We only want to stream the usb devices. This flag is set if the previous
 # non-tabbed line started with "USB"
 isUSB=true
 
 # The printf is to ensure at least two trailing newlines.
 # This is important to the loop.
-# printf "$(v4l2-ctl --list-devices)\n\n" | while IFS="" ; read -r line ; do
-printf "$(cat log.txt)\n\n" | while IFS="" ; read -r line ; do
+printf "$(v4l2-ctl --list-devices)\n\n" | while IFS="" ; read -r line ; do
+# printf "$(cat log.txt)\n\n" | while IFS="" ; read -r line ; do
   if [[ "$line" = $'\t'* ]]; then
     if [ $isUSB = true ]; then
       # xargs trims the tab off the front of the line
