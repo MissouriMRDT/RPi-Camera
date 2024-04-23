c03253dd2567976fc712ce62d35f4340cf7d00b0
Robert Boerwinkle <rfboerwinkle@gmail.com>
2024-04-22 20:09:13 -0500

    refactoring

--- startOne.sh
+++ startOne.sh
@@ -3,12 +3,17 @@
 # You have to pass in a port number as the first argument.
 
 ip="192.168.100.10"
-video_res="320x240"
-extra_flags=" -loglevel warning"
+
+video_res="-video_size 320x240"
+#video_res=""
+
+extra_flags="-loglevel warning"
+#extra_flags=""
+
 encoding=""
 
 # This loops through all but the first argument (the port).
 for device in "${@:2}" ; do
 #   sleep 1 && echo "starting" $device $1 && sleep 8
-  ffmpeg $extra_flags -video_size $video_res -i "$device" $encoding -f mpegts udp://$ip:$1
+  ffmpeg $extra_flags $video_res -i "$device" $encoding -f mpegts udp://$ip:$1
 done
